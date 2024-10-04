06a4ce388a67e3548db0b080d90d585ead78bede
Vidhi <vidhi2005ajmera@gmail.com>
2024-10-05 01:12:04 +0530

    Frontend Deploy

---
+++ src/utils/constant.js
@@ -0,0 +1,10 @@
+export const USER_API_END_POINT =
+  "https://jp-backend-2.onrender.com/api/v1/user";
+
+export const JOB_API_END_POINT = "https://jp-backend-2.onrender.com/api/v1/job";
+
+export const APPLICATION_API_END_POINT =
+  "https://jp-backend-2.onrender.com/api/v1/application";
+
+export const COMPANY_API_END_POINT =
+  "https://jp-backend-2.onrender.com/api/v1/company";
